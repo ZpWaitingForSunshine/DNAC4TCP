1f99162d81ff916b82c6f588bf3c30d4f4e1929d
zp <penn0121@163.com>
2023-10-12 22:25:34 +0800

    ST format for reduce communication

--- test2.py
+++ test2.py
@@ -1,19 +1,26 @@
-import numpy as np
-from scipy.sparse import csr_matrix
-
-# 创建一个密集矩阵
-dense_matrix = np.array([[0, 3, 0], [0, 0, 0], [0, 0, 0]])
-
-# 将密集矩阵转换为CSR格式的稀疏矩阵
-sparse_matrix = csr_matrix(dense_matrix)
-
-print("原始密集矩阵:")
-print(dense_matrix)
-
-print("\n转换后的稀疏矩阵 (CSR格式):")
-print(sparse_matrix)
+import cv2
+import time
 
 
-c = csr_matrix((sparse_matrix.data, sparse_matrix.indices,
-                                      sparse_matrix.indptr)).toarray()
-print(c)
+def main(file_path):
+    cap = cv2.cudacodec.createVideoReader(file_path)
+    # i = 1
+    start_time = time.time()
+    while True:
+        ret, frame = cap.nextFrame()
+        if ret is False:
+            break
+    # frame_cuda = frame.download()
+    # print(i)
+    # i = i + 1
+    # cv2.imshow('image', frame_cuda)
+    # cv2.waitKey(1)
+    end_time = time.time()
+    exe_time = end_time - start_time
+    print("time:", exe_time)
+    cap = None
+
+
+if __name__ == '__main__':
+    file_path = 'video.mp4'
+    main(file_path)
